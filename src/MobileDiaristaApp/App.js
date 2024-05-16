3719e01c734808afd24c98d051a5ba74345b16c7
santanagabi <gaboliveirasantana1@gmail.com>
2024-05-16 20:52:38 -0300

    Ajustado pasta do mobile

---
+++ src/MobileDiaristaApp/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.gabi}> Testeeeeeeeeeeeeeeeeee Gabi</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gabi: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+});
