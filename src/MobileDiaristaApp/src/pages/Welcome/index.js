d4f481fb92efc070e4a158d82a448b2d5fc5180e
santanagabi <gaboliveirasantana1@gmail.com>
2024-05-16 21:15:40 -0300

    Ajustando a arquitetura do projeto

---
+++ src/MobileDiaristaApp/src/pages/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from  'react-native'
+
+export default function Welcome () {
+    return (
+        <View>
+            <Text>Bem vindo</Text>
+        </View>
+    )
+}
